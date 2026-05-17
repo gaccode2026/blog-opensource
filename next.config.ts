607ca7f5b0628330f763ed1adece490386f23057
gaccode2026 <1075070660@qq.com>
2026-05-17 15:32:41 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-// 这是纯 JS 版本，绝对不会报错
 module.exports = {
+  // 开启静态导出，让 Next.js 生成纯静态文件到 out 目录
+  output: "export",
+  // 跳过 TS 检查，保证构建成功
   typescript: {
     ignoreBuildErrors: true,
   },
+  // 跳过 ESLint 检查（可选）
   eslint: {
     ignoreDuringBuilds: true,
   },
